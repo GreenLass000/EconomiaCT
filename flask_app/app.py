d9ee280f80567eba6bbe9aca0059c311fa287f3b
GreenLass <marcosnavarromartin@gmail.com>
2024-09-10 14:16:05 +0200

    Added origin 192.168.1.130 to CORS

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 CORS(app, resources={
      r"/*": {"origins": [
          "http://localhost:3000",
-         "http://83.58.177.180:3000",
-         "http://26.43.27.30:3000"
+         "http://192.168.1.130:3000",
      ]}})
 
 # Inicializar la base de datos
